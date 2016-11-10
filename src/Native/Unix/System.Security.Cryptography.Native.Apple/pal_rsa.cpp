584f9e52e8bdfa4f3996b9859c098714c221600b
Jeremy Barton <jbarton@microsoft.com>
2016-11-09 20:15:55 -0800

    Consolidate sectransform.cpp into pal_rsa, as the only caller

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_rsa.cpp
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_rsa.cpp
@@ -3,9 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 #include "pal_rsa.h"
-#include <stdio.h>
 
-int32_t ExecuteCFDataTransform(
+static int32_t ExecuteCFDataTransform(
     SecTransformRef xform, uint8_t* pbData, int32_t cbData, CFDataRef* pDataOut, CFErrorRef* pErrorOut);
 
 extern "C" int
@@ -217,6 +216,63 @@ cleanup:
     if (encryptor != nullptr)
     {
         CFRelease(encryptor);
+    }
+
+    return ret;
+}
+
+static int32_t ExecuteCFDataTransform(
+    SecTransformRef xform, uint8_t* pbData, int32_t cbData, CFDataRef* pDataOut, CFErrorRef* pErrorOut)
+{
+    if (xform == nullptr || pbData == nullptr || cbData < 0 || pDataOut == nullptr || pErrorOut == nullptr)
+    {
+        return kErrorBadInput;
+    }
+
+    *pDataOut = nullptr;
+    *pErrorOut = nullptr;
+
+    CFTypeRef xformOutput = nullptr;
+    CFDataRef cfData = nullptr;
+    int ret = INT_MIN;
+
+    cfData = CFDataCreateWithBytesNoCopy(nullptr, pbData, cbData, kCFAllocatorNull);
+
+    if (!SecTransformSetAttribute(xform, kSecTransformInputAttributeName, cfData, pErrorOut))
+    {
+        ret = kErrorSeeError;
+        goto cleanup;
+    }
+
+    xformOutput = SecTransformExecute(xform, pErrorOut);
+
+    if (xformOutput == nullptr || *pErrorOut != nullptr)
+    {
+        ret = kErrorSeeError;
+        goto cleanup;
+    }
+
+    if (CFGetTypeID(xformOutput) == CFDataGetTypeID())
+    {
+        CFDataRef cfDataOut = reinterpret_cast<CFDataRef>(const_cast<void*>(xformOutput));
+        CFRetain(cfDataOut);
+        *pDataOut = cfDataOut;
+        ret = 1;
+    }
+    else
+    {
+        ret = kErrorUnknownState;
+    }
+
+cleanup:
+    if (xformOutput != nullptr)
+    {
+        CFRelease(xformOutput);
+    }
+
+    if (cfData != nullptr)
+    {
+        CFRelease(cfData);
     }
 
     return ret;
