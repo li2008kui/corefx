aa8fefbae470a6c403995011841539fbc02a32f6
Jeremy Barton <jbarton@microsoft.com>
2016-11-19 21:50:18 -0800

    Loading a certificate from bytes and recovering those bytes works

---
+++ src/Common/src/Interop/OSX/System.Security.Cryptography.Native.Apple/Interop.X509.cs
@@ -0,0 +1,104 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+using Microsoft.Win32.SafeHandles;
+
+internal static partial class Interop
+{
+    internal static partial class AppleCrypto
+    {
+        [DllImport(Libraries.AppleCryptoNative)]
+        private static extern int AppleCryptoNative_X509ImportCertificate(
+            byte[] pbKeyBlob,
+            int cbKeyBlob,
+            out SafeSecCertificateHandle pCertOut,
+            out SafeCreateHandle pPrivateKeyOut,
+            out int pOSStatus);
+
+        [DllImport(Libraries.AppleCryptoNative)]
+        private static extern int AppleCryptoNative_X509GetRawData(
+            SafeSecCertificateHandle cert,
+            out SafeCFDataHandle cfDataOut,
+            out int pOSStatus);
+
+        internal static byte[] X509GetRawData(SafeSecCertificateHandle cert)
+        {
+            int osStatus;
+            SafeCFDataHandle data;
+
+            int ret = AppleCryptoNative_X509GetRawData(
+                cert,
+                out data,
+                out osStatus);
+
+            if (ret == 1)
+            {
+                return CoreFoundation.CFGetData(data);
+            }
+
+            if (ret == 0)
+            {
+                throw CreateExceptionForCCError(osStatus, OSStatus);
+            }
+
+            Debug.Fail($"Unexpected return value {ret}");
+            throw new CryptographicException();
+        }
+
+        internal static SafeSecCertificateHandle X509ImportCertificate(
+            byte[] bytes,
+            out SafeCreateHandle privateKey)
+        {
+            SafeSecCertificateHandle certHandle;
+            int osStatus;
+
+            int ret = AppleCryptoNative_X509ImportCertificate(
+                bytes,
+                bytes.Length,
+                out certHandle,
+                out privateKey,
+                out osStatus);
+
+            if (ret == 1)
+            {
+                return certHandle;
+            }
+
+            certHandle.Dispose();
+            privateKey.Dispose();
+
+            if (ret == 0)
+            {
+                throw CreateExceptionForCCError(osStatus, OSStatus);
+            }
+
+            Debug.Fail($"Unexpected return value {ret}");
+            throw new CryptographicException();
+        }
+    }
+}
+
+namespace System.Security.Cryptography.X509Certificates
+{
+    internal class SafeSecCertificateHandle : SafeHandle
+    {
+        public SafeSecCertificateHandle()
+            : base(IntPtr.Zero, ownsHandle: true)
+        {
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            Interop.CoreFoundation.CFRelease(handle);
+            return true;
+        }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+    }
+}
