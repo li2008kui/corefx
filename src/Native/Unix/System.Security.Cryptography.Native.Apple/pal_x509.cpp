ee1ddab0dfafb27dce5fa521cb96058e929c23f8
Jeremy Barton <jbarton@microsoft.com>
2016-11-19 21:44:51 -0800

    Native code for import/export x509

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509.cpp
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509.cpp
@@ -4,14 +4,115 @@
 
 #include "pal_x509.h"
 
-
-extern "C" int32_t AppleCrypto_GetX509RawData(SecCertificateRef cert, CFDataRef* pDataOut)
+extern "C" int32_t AppleCryptoNative_X509ImportCertificate(
+    uint8_t* pbData, int32_t cbData, SecCertificateRef* pCertOut, SecKeyRef* pPrivateKeyOut, int32_t* pOSStatus)
 {
-    if (pDataOut != nullptr)
-        *pDataOut = nullptr;
+    if (pCertOut != nullptr)
+        *pCertOut = nullptr;
+    if (pPrivateKeyOut != nullptr)
+        *pPrivateKeyOut = nullptr;
+    if (pOSStatus != nullptr)
+        *pOSStatus = noErr;
 
-    if (cert == nullptr || pDataOut == nullptr)
+    if (pbData == nullptr || cbData < 0 || pCertOut == nullptr || pPrivateKeyOut == nullptr)
+    {
+        return kErrorBadInput;
+    }
+
+    CFDataRef cfData = CFDataCreateWithBytesNoCopy(nullptr, pbData, cbData, kCFAllocatorNull);
+
+    if (cfData == nullptr)
+    {
+        return kErrorUnknownState;
+    }
+
+    SecExternalFormat dataFormat = kSecFormatUnknown;
+
+    SecExternalItemType itemType = kSecItemTypeCertificate;
+    SecExternalItemType actualType = itemType;
+
+    int32_t ret = 0;
+    CFIndex itemCount;
+    CFArrayRef outItems = nullptr;
+    CFTypeRef outItem = nullptr;
+
+    *pOSStatus = SecItemImport(cfData, nullptr, &dataFormat, &actualType, 0, nullptr, nullptr, &outItems);
+
+    if (*pOSStatus != noErr)
+    {
+        ret = 0;
+        goto cleanup;
+    }
+
+    if (outItems == nullptr)
+    {
+        ret = -1;
+        goto cleanup;
+    }
+
+    itemCount = CFArrayGetCount(outItems);
+
+    if (itemCount == 0)
+    {
+        ret = -2;
+        goto cleanup;
+    }
+
+    if (actualType == kSecItemTypeCertificate)
+    {
+        if (itemCount > 1)
+        {
+            ret = -4;
+            goto cleanup;
+        }
+
+        outItem = CFArrayGetValueAtIndex(outItems, 0);
+
+        if (outItem == nullptr)
+        {
+            ret = -5;
+            goto cleanup;
+        }
+
+        if (CFGetTypeID(outItem) != SecCertificateGetTypeID())
+        {
+            ret = -6;
+            goto cleanup;
+        }
+
+        CFRetain(outItem);
+        *pCertOut = reinterpret_cast<SecCertificateRef>(const_cast<void*>(outItem));
+        ret = 1;
+    }
+    else
+    {
+        ret = -3;
+    }
+
+cleanup:
+    if (outItems != nullptr)
+    {
+        CFRelease(outItems);
+    }
+
+    CFRelease(cfData);
+    return ret;
+}
+
+extern "C" int32_t AppleCryptoNative_X509GetRawData(SecCertificateRef cert, CFDataRef* ppDataOut, int32_t* pOSStatus)
+{
+    if (ppDataOut != nullptr)
+        *ppDataOut = nullptr;
+    if (pOSStatus != nullptr)
+        *pOSStatus = noErr;
+
+    if (cert == nullptr || ppDataOut == nullptr || pOSStatus == nullptr)
         return kErrorBadInput;
 
-    return kErrorUnknownState;
+    SecExternalFormat dataFormat = kSecFormatX509Cert;
+    SecItemImportExportKeyParameters keyParams = {};
+    keyParams.version = SEC_KEY_IMPORT_EXPORT_PARAMS_VERSION;
+
+    *pOSStatus = SecItemExport(cert, dataFormat, 0, &keyParams, ppDataOut);
+    return (*pOSStatus == noErr);
 }
