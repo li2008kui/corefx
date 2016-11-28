3c6449434d79bdb6b0ed1ae0a41dc7b1fc207f73
Jeremy Barton <jbarton@microsoft.com>
2016-11-28 11:58:11 -0800

    Native work for X509 so far.  About to rewrit blob loading to handle PKCS12 and PKCS7

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509.cpp
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509.cpp
@@ -4,6 +4,180 @@
 
 #include "pal_x509.h"
 
+extern "C" int32_t AppleCryptoNative_X509GetPublicKey(SecCertificateRef cert, SecKeyRef* pPublicKeyOut, int32_t* pOSStatusOut)
+{
+    if (pPublicKeyOut != nullptr)
+        *pPublicKeyOut = nullptr;
+    if (pOSStatusOut != nullptr)
+        *pOSStatusOut = noErr;
+
+    if (cert == nullptr || pPublicKeyOut == nullptr || pOSStatusOut == nullptr)
+        return kErrorBadInput;
+
+    *pOSStatusOut = SecCertificateCopyPublicKey(cert, pPublicKeyOut);
+    return (*pOSStatusOut == noErr);
+}
+
+extern "C" PAL_X509ContentType AppleCryptoNative_X509GetContentType(uint8_t* pbData, int32_t cbData)
+{
+    if (pbData == nullptr || cbData < 0)
+        return PAL_X509Unknown;
+
+    CFDataRef cfData = CFDataCreateWithBytesNoCopy(nullptr, pbData, cbData, kCFAllocatorNull);
+
+    if (cfData == nullptr)
+        return PAL_X509Unknown;
+
+    SecExternalFormat dataFormat = kSecFormatUnknown;
+    SecExternalItemType itemType = kSecItemTypeCertificate;
+    SecExternalItemType actualType = itemType;
+
+    OSStatus osStatus = SecItemImport(cfData, nullptr, &dataFormat, &actualType, 0, nullptr, nullptr, nullptr);
+
+    printf("Call 1: osStatus (%d), dataFormat (%d), actualType (%d)\n", osStatus, dataFormat, actualType);
+
+    if (osStatus == noErr)
+    {
+        if (actualType == kSecItemTypeCertificate)
+        {
+            return PAL_Certificate;
+        }
+        else if (actualType == kSecItemTypeAggregate)
+        {
+            if (dataFormat == kSecFormatPKCS7)
+            {
+                return PAL_Pkcs7;
+            }
+
+            if (dataFormat == kSecFormatPKCS12)
+            {
+                return PAL_Pkcs12;
+            }
+
+            printf("DataFormat: %d\n", dataFormat);
+        }
+
+        printf("ActualType was not certificate: %d\n", actualType);
+    }
+    else if (osStatus != errSecUnsupportedFormat) { printf("Call 1: %d\n", osStatus); }
+
+    actualType = kSecItemTypeAggregate;
+    dataFormat = kSecFormatPKCS7;
+
+    osStatus = SecItemImport(cfData, nullptr, &dataFormat, &actualType, 0, nullptr, nullptr, nullptr);
+
+    printf("Call 2: osStatus (%d), dataFormat (%d), actualType (%d)\n", osStatus, dataFormat, actualType);
+
+    if (osStatus == noErr)
+    {
+        if (actualType == kSecItemTypeAggregate && dataFormat == kSecFormatPKCS7)
+        {
+            return PAL_Pkcs7;
+        }
+
+        printf("ActualType (%d), dataFormat (%d)\n", actualType, dataFormat);
+    }
+    else if (osStatus != errSecUnsupportedFormat) { printf("Call 2: %d\n", osStatus); }
+
+    actualType = kSecItemTypeAggregate;
+    dataFormat = kSecFormatPKCS12;
+
+    osStatus = SecItemImport(cfData, nullptr, &dataFormat, &actualType, 0, nullptr, nullptr, nullptr);
+
+    printf("Call 3: osStatus (%d), dataFormat (%d), actualType (%d)\n", osStatus, dataFormat, actualType);
+
+    if (osStatus == noErr || osStatus == errSecPassphraseRequired)
+    {
+        if (actualType == kSecItemTypeAggregate && dataFormat == kSecFormatPKCS12)
+        {
+            return PAL_Pkcs12;
+        }
+
+        printf("ActualType (%d), dataFormat (%d)\n", actualType, dataFormat);
+    }
+    else if (osStatus != errSecUnsupportedFormat) { printf("Call 3: %d\n", osStatus); }
+
+    return PAL_X509Unknown;
+}
+
+#if 0
+static int32_t ProcessCertificateTypeReturn(SecExternalItemType type, CFArrayRef items, SecCertificateRef* pCertOut, SecKeyRef* pPrivateKeyOut)
+{
+    CFIndex itemCount = CFArrayGetCount(items);
+
+    if (itemCount == 0)
+    {
+        return -10;
+    }
+
+    if (type == kSecItemTypeCertificate)
+    {
+        CFTypeRef item = CFArrayGetValueAtIndex(items, 0);
+
+        if (item == nullptr)
+        {
+            return -11;
+        }
+
+        if (CFGetTypeID(item) != SecCertificateGetTypeID())
+        {
+            return -12;
+        }
+
+        CFRetain(item);
+        *pCertOut = reinterpret_cast<SecCertificateRef>(const_cast<void*>(item));
+        return 1;
+    }
+    else if (type == kSecItemTypeAggregate)
+    {
+        CFTypeRef bestItem = nullptr;
+
+        printf("Dictionary Type: %lu\n", CFDictionaryGetTypeID());
+        printf("Certificate Type: %lu\n", SecCertificateGetTypeID());
+        printf("Identity Type: %lu\n", SecIdentityGetTypeID());
+
+        for (CFIndex i = 0; i < itemCount; i++)
+        {
+            CFTypeRef current = CFArrayGetValueAtIndex(items, i);
+            auto currentItemType = CFGetTypeID(current);
+
+            printf("Item %lu: %lu\n", i, currentItemType);
+
+            if (currentItemType == SecIdentityGetTypeID())
+            {
+                bestItem = current;
+                break;
+            }
+            else if (bestItem == nullptr && currentItemType == SecCertificateGetTypeID())
+            {
+                bestItem = current;
+                break;
+            }
+        }
+
+        if (bestItem == nullptr)
+        {
+            return -13;
+        }
+
+        if (CFGetTypeID(bestItem) == SecCertificateGetTypeID())
+        {
+            CFRetain(bestItem);
+            *pCertOut = reinterpret_cast<SecCertificateRef>(const_cast<void*>(bestItem));
+            return 1;
+        }
+
+        if (CFGetTypeID(bestItem) == SecIdentityGetTypeID())
+        {
+            //SecIdentityRef identity = reinterpret_cast<SecIdentityRef>(const_cast<void*>(bestItem));
+            *pPrivateKeyOut = nullptr;
+        }
+    }
+
+    return 0;
+}
+#endif
+
 extern "C" int32_t AppleCryptoNative_X509ImportCertificate(
     uint8_t* pbData, int32_t cbData, SecCertificateRef* pCertOut, SecKeyRef* pPrivateKeyOut, int32_t* pOSStatus)
 {
