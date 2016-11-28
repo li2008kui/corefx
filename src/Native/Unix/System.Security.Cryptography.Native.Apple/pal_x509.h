0b99313d67dfd9707ebb8d4eb622a8774b1fd3c5
Jeremy Barton <jbarton@microsoft.com>
2016-11-28 11:58:11 -0800

    Native work for X509 so far.  About to rewrit blob loading to handle PKCS12 and PKCS7

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509.h
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509.h
@@ -8,3 +8,15 @@
 #include "pal_seckey.h"
 
 #include <Security/Security.h>
+
+enum
+{
+    PAL_X509Unknown = 0,
+    PAL_Certificate = 1,
+    PAL_SerializedCert = 2,
+    PAL_Pkcs12 = 3,
+    PAL_SerializedStore = 4,
+    PAL_Pkcs7 = 5,
+    PAL_Authenticode = 6,
+};
+typedef uint32_t PAL_X509ContentType;
