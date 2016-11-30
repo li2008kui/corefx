04a0032e4417215eed29fccd1b3154f489d417ae
Jeremy Barton <jbarton@microsoft.com>
2016-11-29 23:15:41 -0800

    PFX loading, managed side

--- src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.OSX/CertificatePal.cs
+++ src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.OSX/CertificatePal.cs
@@ -33,7 +33,7 @@ namespace Internal.Cryptography.Pal
             SafeCreateHandle privateKey;
 
             SafeSecCertificateHandle certHandle =
-                Interop.AppleCrypto.X509ImportCertificate(rawData, out privateKey);
+                Interop.AppleCrypto.X509ImportCertificate(rawData, password, out privateKey);
 
             return new AppleCertificatePal(certHandle, privateKey);
         }
